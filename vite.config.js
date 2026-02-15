70e3bb4f70bb09a93d01aea31e4b71309ce0de2a
Suman Saha <8759127658itisaha@gmail.com>
2026-02-15 19:34:15 +0530

    fix: Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: '/project-deadpool/',
   plugins: [
     tailwindcss(),
   ],
